fc53141fc0324c94b3d0a67294563f5777ff8a98
Romero Cartaxo <romero.cartaxo@solyd.com.br>
2024-07-31 10:03:22 -0300

    update views.py

--- views.py
+++ views.py
@@ -3,8 +3,10 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 
+from tableau import create_tableau, A, b, c
 
 bp = Blueprint('diet-problem', __name__, url_prefix='/')
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    initial_tableau = create_tableau(A, b, c)
+    return render_template('index.html', tableau=initial_tableau)
