4f517928cdf02d2cdfff3cd5a6502308e7436560
Romero Cartaxo <romero.cartaxo@solyd.com.br>
2024-08-01 01:32:55 -0300

    finish implementation

--- tableau.py
+++ tableau.py
@@ -33,8 +33,8 @@ def create_tableau(A, b, c):
 #     # tableau.extend(identity_matrix)
 #     tableau = transpose(tableau)
 #     for row in range(len(tableau)):
-#         for col in range(len(tableau[row])):
-#             if row == col:
+#         for pivot_col in range(len(tableau[row])):
+#             if row == pivot_col:
 #                 tableau[row].append(1)
 #             else:
 #                 tableau[row].append(0)
@@ -51,13 +51,13 @@ def tableau_has_finished(tableau):
             return False
     return True
 
-def get_next_tableau_part1(tableau):
-    col = aux.argmin(tableau[-1])
+def get_next_tableau(tableau):
+    pivot_col = aux.argmin(tableau[-1])
     
     ratios = []
     for row in range(len(tableau) - 1):
-        if tableau[row][col] > 0:
-            ratios.append(tableau[row][-1] / tableau[row][col])
+        if tableau[row][pivot_col] > 0:
+            ratios.append(tableau[row][-1] / tableau[row][pivot_col])
         else:
             ratios.append(float('inf'))  
     
@@ -67,34 +67,74 @@ def get_next_tableau_part1(tableau):
     
 
     #encontrando a linha do pivo
-    pivot_index = (aux.argmin(ratios), col)
-    return pivot_index
-
-def get_next_tableau_part2(tableau, pivot_index):
+    pivot_row = aux.argmin(ratios)
 
     #normalizar a linha do pivo
-    pivot_elem = tableau[pivot_index[0]][pivot_index[1]]
-    tableau[pivot_index[0]]= [x/pivot_elem for x in tableau[pivot_index[0]]]
+    pivot_elem = tableau[pivot_row][pivot_col]
+    tableau[pivot_row]= [x/pivot_elem for x in tableau[pivot_row]]
 
     for row in range(len(tableau)):
-        if row == pivot_index[0]:
+        if row == pivot_row:
             continue
         current_row = tableau[row]
-        coefficient = tableau[row][pivot_index[1]]
-        pivot_row_multiplied = [x*-coefficient for x in tableau[pivot_index[0]]]
+        coefficient = tableau[row][pivot_col]
+        pivot_row_multiplied = [x*-coefficient for x in tableau[pivot_row]]
 
         new_row = aux.sum_lists(current_row, pivot_row_multiplied)
         tableau[row] = new_row
-    return tableau
+        
+    print(tableau)
+    print('\n\n')
+    return ratios, pivot_col, pivot_row
+
+
+def solve_simplex(tableau):
+    # tableaus_data = {
+    # 'tableaus': [],
+    # 'tableau_headers': [],
+    # 'tableau_basis': [],
+    # 'pivot_column': [],
+    # 'pivot_row': [],
+    # 'tableau_TRs': [],
+    # }
+    headers = [f'y{i}' for i in range(len(data.b))]
+    headers.extend([f'x{i}' for i in range(len(data.c))])
+    headers.extend(['P', 'Solution', 'TR'])
+    
+    basis = [f'x{i}' for i in range(len(data.c))]
+    basis.append('P')
+    
+    tableau_copy = deepcopy(tableau)
+    tableaus_data = [
+        {'tableau': tableau_copy, 'headers': headers, 'basis': basis, 'tr': None}
+    ]
+
+    tableau_copy = deepcopy(tableau)
+    tableaus_data.append({'tableau': tableau_copy, 'headers': headers, 'basis': basis, 'tr': None})
+
+    while not tableau_has_finished(tableau):
+        ratios, pivot_header, pivot_basis = get_next_tableau(tableau)
+        tableau_copy = deepcopy(tableau)
+        basis_copy = basis[:]
+        basis_copy[pivot_basis] = headers[pivot_header]
+        tableaus_data[-1].update({
+            'pivot_header': pivot_header, 'pivot_basis': pivot_basis, 'tr': ratios
+        })
+        tableaus_data.append({'tableau': tableau_copy, 'headers': headers, 'basis': basis_copy, 'tr': None})
+
+        
+    for i,x in enumerate(tableau[-1][2:]):
+        print(f"x{i}: {x}")
+    return tableaus_data
+
 
 tableau = create_tableau(data.A, data.b, data.c)
-# tableau = create_tableau2(A, b, c)
+tableaus_data = solve_simplex(tableau)
 
-while not tableau_has_finished(tableau):
-    pivot_index = get_next_tableau_part1(tableau)
-    get_next_tableau_part2(tableau, pivot_index)
-
-for i,x in enumerate(tableau[-1][21:]):
-    print(f"x{i}: {x}")
-
-    
+last_tableau = tableaus_data[-1]["tableau"]
+portions = last_tableau[-1][len(data.b): len(data.b)+len(data.c)]
+tableau_summary = {
+    'cost': f'R$ {last_tableau[-1][-1] / 100:.2f}'.replace('.',','),
+    'foods': {data.food_names[i]: round(portions[i]*data.food_portions[i]) for i in range(len(data.food_names))}
+}
+print(tableau_summary)
--- views.py
+++ views.py
@@ -3,10 +3,13 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 
-from tableau import create_tableau, A, b, c
+from tableau import create_tableau
+from tableau import tableaus_data
+from tableau import tableau_summary
+from data import A, b, c
 
 bp = Blueprint('diet-problem', __name__, url_prefix='/')
 @bp.route('/')
 def index():
     initial_tableau = create_tableau(A, b, c)
-    return render_template('index.html', tableau=initial_tableau)
+    return render_template('index.html', data=tableaus_data, summary=tableau_summary)
